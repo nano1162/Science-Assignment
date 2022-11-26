1354fa8ff22a3212cc5a455268eebaa812444355
강동혁 <flynano1162@gmail.com>
2022-11-26 21:54:32 +0900

    텍스트로서 제작 완료.

--- made.py
+++ made.py
@@ -3,34 +3,21 @@ import matplotlib.pyplot as plt
 import math
 import matplotlib.animation as animation
 
-print('나는 구현할 수 있습니다. ')
-y = float(input('출발지점 Y좌표를 입력해 주세요. : ')) #높이 입력
+print('나는 구현할 수 있습니다. ') # 의지 표명
+y = float(input('출발지점 Y좌표를 입력해 주세요. : ')) # 높이 입력
+g = 9.8 #중력가속도
 
-v = 0
+print('최초 높이:', y) # 최초 높이 표시
 
-print('최초 높이:', y)
+t = math.sqrt(2*y/g)
+timetable = np.linspace(0, t, 100) # 떨어지는데 필요한 시간 계산
 
-t = np.linspace(0, 10, 100)
+def func(time1): # 시간 당 거리 계산
+    S = 4.9*(time1**2)
+    return S
 
-S = 1/2*9.8*(time**2)
+for i in range(len(timetable)): # 시간 당 높이 계산
+    a = y - func(timetable[i])
+    print('높이 :', a) # 시간 당 높이 표시
 
-timetable = []
-for i in range(len(t)):
-    timetable.append(func(t[i]))
-
-print(timetable)
-
-for i in range(len(timetable)):
-    y = y - timetable[i]
-    print('높이 :', y)
-
-
-
-''' 
-plt.figure()
-plt.plot(x,y, 'b-', label = '1st')
-plt.ylabel('Y')
-plt.xlabel('X')
-plt.legend(loc='best')
-plt.show() 
-'''
+print('걸린 시간 :', t, '초') # 걸린 시간
