ca47a0137e6e0b31abb5c9dedfc73ecf7b410b5e
강동혁 <flynano1162@gmail.com>
2022-11-26 21:36:18 +0900

    뭐가 뭔지 전혀 모르겠음. 수정 진행 중

--- made.py
+++ made.py
@@ -3,16 +3,34 @@ import matplotlib.pyplot as plt
 import math
 import matplotlib.animation as animation
 
-print('(100, 100)까지 구현할 수 있습니다. ')
-x = int(input('출발지점 X좌표를 입력해 주세요. : '))
-y = int(input('출발지점 Y좌표를 입력해 주세요. : '))
+print('나는 구현할 수 있습니다. ')
+y = float(input('출발지점 Y좌표를 입력해 주세요. : ')) #높이 입력
 
+v = 0
+
+print('최초 높이:', y)
 
 t = np.linspace(0, 10, 100)
 
+S = 1/2*9.8*(time**2)
+
+timetable = []
+for i in range(len(t)):
+    timetable.append(func(t[i]))
+
+print(timetable)
+
+for i in range(len(timetable)):
+    y = y - timetable[i]
+    print('높이 :', y)
+
+
+
+''' 
 plt.figure()
 plt.plot(x,y, 'b-', label = '1st')
 plt.ylabel('Y')
 plt.xlabel('X')
 plt.legend(loc='best')
-plt.show()
+plt.show() 
+'''
