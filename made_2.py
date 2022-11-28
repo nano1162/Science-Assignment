51a21efb0e1b085bd2952cacd68054254a88a651
강동혁 <flynano1162@gmail.com>
2022-11-28 19:53:24 +0900

    완벽하다. 등가속도 직선 운동 구현 완료 및 텍스트 추가

--- made_2.py
+++ made_2.py
@@ -8,6 +8,8 @@ from celluloid import Camera
 # 텍스트 구현
 
 y = float(input('출발지점 Y좌표를 입력해 주세요. : ')) # 높이 입력
+x = float(input('출발 속도를 입력해 주세요. : '))
+z = float(input('수평 방향 가속도를 입력해 주세요. : '))
 g = 9.8 #중력가속도
 
 t = math.sqrt(2*y/g)
@@ -17,20 +19,27 @@ def func(time1): # 시간 당 거리 계산
     S = 4.9*(time1**2)
     return S
 
+def two(time1): # 시간 당 거리 계산
+    S = (x * time1) + ((z/2)*(time1**2))
+    return S
+
 fig, axes = plt.subplots(1)
-plt.ylabel('Height')
 
 camera = Camera(fig)
 
 a = []
+b = []
 
 for i in range(len(timetable)): # 시간 당 높이 계산
     a.append(y - func(timetable[i]))
+    b.append(x + two(timetable[i]))
     # print('높이 :', a[i]) # 시간 당 높이 표시
-    axes.plot(1, a[i], 'o', markersize = 20, color = 'r')
-    plt.xlabel('Time = %1fs' %timetable[i])
+    axes.plot(b[i], a[i], 'o', markersize = 20, color = 'r')
+    axes.text(b[0],0,'Time = %1fs' %timetable[i])
+    axes.text(b[i], a[i], 'Height = %1fm' %a[i])
+    axes.text( b[0] , y / 2 , 'Distance = %1fm' %b[i])
     camera.snap()
 
 animation = camera.animate(interval=50)
 
-animation.save( 'gravity.gif', writer = 'ffmpef', fps = 60)
+animation.save( 'gravity.gif', writer = 'ffmpef', fps = 20)
